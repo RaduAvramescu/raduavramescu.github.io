2df9b063836e41f6da288d7822d9dd7e59005cc1
RaduAvramescu <71271545+RaduAvramescu@users.noreply.github.com>
2020-10-26 10:41:00 +0200

    Improved hierarchy of files, changed identation, and make projectFunction pull data from JSON

--- js/main.js
+++ js/main.js
@@ -3,23 +3,27 @@
 let lastScroll = top;
 
 function isScrolling() {
-  window.onscroll = function() {
-      let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-      if (currentScroll > 0 && lastScroll <= currentScroll) {
-        lastScroll = currentScroll;
-        header.classList.add("header-hide");
-      } else {
-        lastScroll = currentScroll;
-        if (document.getElementsByTagName('header')[0].classList.contains("header-hide"))
-          {
-            header.classList.remove("header-hide");
-            nav.setAttribute("style", "transition-property: none;");
-            nav.classList.remove("show");
-            setTimeout(() => {
-              nav.removeAttribute("style");
-            }, 100);
-          }
+  window.onscroll = function () {
+    let currentScroll =
+      document.documentElement.scrollTop || document.body.scrollTop;
+    if (currentScroll > 0 && lastScroll <= currentScroll) {
+      lastScroll = currentScroll;
+      header.classList.add("header-hide");
+    } else {
+      lastScroll = currentScroll;
+      if (
+        document
+          .getElementsByTagName("header")[0]
+          .classList.contains("header-hide")
+      ) {
+        header.classList.remove("header-hide");
+        nav.setAttribute("style", "transition-property: none;");
+        nav.classList.remove("show");
+        setTimeout(() => {
+          nav.removeAttribute("style");
+        }, 100);
       }
+    }
   };
 }
 
@@ -33,86 +37,24 @@ function toggleNav() {
   nav.classList.toggle("show");
 }
 
-// Project list
-
-function projectFunction(projectArr) {
-  let output = "";
-  let i;
-  for(i = 0; i < projectArr.length; i++) {
-      if (i % 3 == 0) {
-          output += '<div class="row">';
-      }
-      output += '<div class="col-lg-4 slide-in from-left opacity-0">' +
-      '<div class="card mb-5" id="' + i + '">' +
-      '<div class="overflow-hidden">' +
-      '<a data-toggle="modal" data-target="#myModal" onclick="modalOpen' +
-      '(' + i + ');">' +
-      '<img src="' + projectArr[i].projectImage + '" alt="' + projectArr[i].projectTitle + '" class="card-img-top w-100">' +
-      '</a>' +
-      '</div>' +
-      '<div class="card-body">' +
-      '<h5 class="card-title">' + projectArr[i].projectTitle + '</h5>' +
-      '<p class="card-text mb-3">' + projectArr[i].projectDescription + '</p>' +
-      '<div class="container mb-3">' +
-      '<p class="font-weight-bold mb-3">TECHNOLOGIES USED</p>' +
-      projectArr[i].projectTechnologies +
-      '</div>' +
-      '<a target="_blank" class="btn btn-primary" href="' + projectArr[i].projectDemoURL + '">DEMO</a> ' +
-      '<a target="_blank" class="btn btn-secondary" href="' + projectArr[i].projectCodeURL + '">CODE</a>' +
-      '</div>' +
-      '</div>' +
-      '</div>';
-      if (i >= 3 && i % 3 == 0) {
-          output += '</div>';
-      }
-  }
-  document.getElementById('projectsContent').innerHTML = output;
-}
-
-let data = [
-  {
-    "projectTitle": "PERSONAL WEBSITE",
-    "projectImage": "img/personal-website.jpg",
-    "projectGif": "img/personal-website.gif",
-    "projectDescription": "Personal website, built to showcase my portfolio.",
-    "projectTechnologies" : '<svg viewBox="0 0 128 128" class="img-projects-tech" width="40"> <path fill="#E44D26" d="M19.037 113.876l-10.005-112.215h109.936l-10.016 112.198-45.019 12.48z"></path><path fill="#F16529" d="M64 116.8l36.378-10.086 8.559-95.878h-44.937z"></path><path fill="#EBEBEB" d="M64 52.455h-18.212l-1.258-14.094h19.47v-13.762h-34.511l.33 3.692 3.382 37.927h30.799zM64 88.198l-.061.017-15.327-4.14-.979-10.975h-13.817l1.928 21.609 28.193 7.826.063-.017z"></path><path fill="#fff" d="M63.952 52.455v13.763h16.947l-1.597 17.849-15.35 4.143v14.319l28.215-7.82.207-2.325 3.234-36.233.335-3.696h-3.708zM63.952 24.599v13.762h33.244l.276-3.092.628-6.978.329-3.692z"></path> <title>HTML5</title> </svg> <svg viewBox="0 0 128 128" class="img-projects-tech" width="40"> <path fill="#1572B6" d="M18.814 114.123l-10.054-112.771h110.48l-10.064 112.754-45.243 12.543-45.119-12.526z"></path><path fill="#33A9DC" d="M64.001 117.062l36.559-10.136 8.601-96.354h-45.16v106.49z"></path><path fill="#fff" d="M64.001 51.429h18.302l1.264-14.163h-19.566v-13.831h34.681999999999995l-.332 3.711-3.4 38.114h-30.95v-13.831z"></path><path fill="#EBEBEB" d="M64.083 87.349l-.061.018-15.403-4.159-.985-11.031h-13.882l1.937 21.717 28.331 7.863.063-.018v-14.39z"></path><path fill="#fff" d="M81.127 64.675l-1.666 18.522-15.426 4.164v14.39l28.354-7.858.208-2.337 2.406-26.881h-13.876z"></path><path fill="#EBEBEB" d="M64.048 23.435v13.831000000000001h-33.407999999999994l-.277-3.108-.63-7.012-.331-3.711h34.646zM64.001 51.431v13.831000000000001h-15.209l-.277-3.108-.631-7.012-.33-3.711h16.447z"></path> <title>CSS3</title> </svg> <svg viewBox="0 0 128 128" class="img-projects-tech" width="40"> <rect x="20" y="20" width="80" height="100" fill="white"/> <path fill="#5B4282" d="M75.701 65.603c-2.334-.768-5.694-.603-10.08-.603h-17.621v23h18.844c2.944 0 5.012-.315 6.203-.535 2.099-.376 3.854-1.104 5.264-1.982 1.409-.876 2.568-2.205 3.478-3.881.908-1.676 1.363-3.637 1.363-5.83 0-2.568-.658-4.54-1.975-6.436-1.316-1.896-3.141-2.965-5.476-3.733zM73.282 55.087c2.317-.688 4.064-1.89 5.239-3.487 1.176-1.598 1.763-3.631 1.763-6.044 0-2.286-.549-4.314-1.646-6.054s-2.662-2.413-4.699-3.056c-2.037-.641-5.53-.446-10.48-.446h-15.459v20h16.587c4.042 0 6.939-.38 8.695-.913zM126 18.625c0-9.182-7.443-16.625-16.625-16.625h-91.75c-9.182 0-16.625 7.443-16.625 16.625v91.75c0 9.182 7.443 16.625 16.625 16.625h91.75c9.182 0 16.625-7.443 16.625-16.625v-91.75zm-35.447 66.12c-1.362 2.773-3.047 4.911-5.052 6.415-2.006 1.504-4.521 2.78-7.544 3.548-3.022.769-6.728 1.292-11.113 1.292h-27.844v-69h27.42c5.264 0 9.485.609 12.665 2.002 3.181 1.395 5.671 3.497 7.474 6.395 1.801 2.898 2.702 5.907 2.702 9.071 0 2.945-.8 5.708-2.397 8.308-1.598 2.602-4.011 4.694-7.237 6.292 4.166 1.222 7.37 3.304 9.61 6.248 2.24 2.945 3.36 6.422 3.36 10.432 0 3.227-.681 6.225-2.044 8.997z"></path> <title>Bootstrap</title> </svg> <svg viewBox="0 0 128 128" class="img-projects-tech" width="40"> <path fill="#F0DB4F" d="M1.408 1.408h125.184v125.185h-125.184z"></path><path fill="#323330" d="M116.347 96.736c-.917-5.711-4.641-10.508-15.672-14.981-3.832-1.761-8.104-3.022-9.377-5.926-.452-1.69-.512-2.642-.226-3.665.821-3.32 4.784-4.355 7.925-3.403 2.023.678 3.938 2.237 5.093 4.724 5.402-3.498 5.391-3.475 9.163-5.879-1.381-2.141-2.118-3.129-3.022-4.045-3.249-3.629-7.676-5.498-14.756-5.355l-3.688.477c-3.534.893-6.902 2.748-8.877 5.235-5.926 6.724-4.236 18.492 2.975 23.335 7.104 5.332 17.54 6.545 18.873 11.531 1.297 6.104-4.486 8.08-10.234 7.378-4.236-.881-6.592-3.034-9.139-6.949-4.688 2.713-4.688 2.713-9.508 5.485 1.143 2.499 2.344 3.63 4.26 5.795 9.068 9.198 31.76 8.746 35.83-5.176.165-.478 1.261-3.666.38-8.581zm-46.885-37.793h-11.709l-.048 30.272c0 6.438.333 12.34-.714 14.149-1.713 3.558-6.152 3.117-8.175 2.427-2.059-1.012-3.106-2.451-4.319-4.485-.333-.584-.583-1.036-.667-1.071l-9.52 5.83c1.583 3.249 3.915 6.069 6.902 7.901 4.462 2.678 10.459 3.499 16.731 2.059 4.082-1.189 7.604-3.652 9.448-7.401 2.666-4.915 2.094-10.864 2.07-17.444.06-10.735.001-21.468.001-32.237z"></path> <title>JavaScript</title> </svg>',
-    "projectDemoURL": "https://raduavramescu.github.io/",
-    "projectCodeURL": "https://github.com/RaduAvramescu/raduavramescu.github.io"
-  },
-  {
-    "projectTitle": "WARHAMMER 2 QUIZ",
-    "projectImage": "https://picsum.photos/400/200",
-    "projectGif": "img/wh2-quiz.gif",
-    "projectDescription": "Quiz game based on Total War: Warhammer 2.",
-    "projectTechnologies" : '<svg viewBox="0 0 128 128" class="img-projects-tech" width="40"> <path fill="#E44D26" d="M19.037 113.876l-10.005-112.215h109.936l-10.016 112.198-45.019 12.48z"></path><path fill="#F16529" d="M64 116.8l36.378-10.086 8.559-95.878h-44.937z"></path><path fill="#EBEBEB" d="M64 52.455h-18.212l-1.258-14.094h19.47v-13.762h-34.511l.33 3.692 3.382 37.927h30.799zM64 88.198l-.061.017-15.327-4.14-.979-10.975h-13.817l1.928 21.609 28.193 7.826.063-.017z"></path><path fill="#fff" d="M63.952 52.455v13.763h16.947l-1.597 17.849-15.35 4.143v14.319l28.215-7.82.207-2.325 3.234-36.233.335-3.696h-3.708zM63.952 24.599v13.762h33.244l.276-3.092.628-6.978.329-3.692z"></path> <title>HTML5</title> </svg> <svg viewBox="0 0 128 128" class="img-projects-tech" width="40"> <path fill="#1572B6" d="M18.814 114.123l-10.054-112.771h110.48l-10.064 112.754-45.243 12.543-45.119-12.526z"></path><path fill="#33A9DC" d="M64.001 117.062l36.559-10.136 8.601-96.354h-45.16v106.49z"></path><path fill="#fff" d="M64.001 51.429h18.302l1.264-14.163h-19.566v-13.831h34.681999999999995l-.332 3.711-3.4 38.114h-30.95v-13.831z"></path><path fill="#EBEBEB" d="M64.083 87.349l-.061.018-15.403-4.159-.985-11.031h-13.882l1.937 21.717 28.331 7.863.063-.018v-14.39z"></path><path fill="#fff" d="M81.127 64.675l-1.666 18.522-15.426 4.164v14.39l28.354-7.858.208-2.337 2.406-26.881h-13.876z"></path><path fill="#EBEBEB" d="M64.048 23.435v13.831000000000001h-33.407999999999994l-.277-3.108-.63-7.012-.331-3.711h34.646zM64.001 51.431v13.831000000000001h-15.209l-.277-3.108-.631-7.012-.33-3.711h16.447z"></path> <title>CSS3</title> </svg> <svg viewBox="0 0 128 128" class="img-projects-tech" width="40"> <rect x="20" y="20" width="80" height="100" fill="white"/> <path fill="#5B4282" d="M75.701 65.603c-2.334-.768-5.694-.603-10.08-.603h-17.621v23h18.844c2.944 0 5.012-.315 6.203-.535 2.099-.376 3.854-1.104 5.264-1.982 1.409-.876 2.568-2.205 3.478-3.881.908-1.676 1.363-3.637 1.363-5.83 0-2.568-.658-4.54-1.975-6.436-1.316-1.896-3.141-2.965-5.476-3.733zM73.282 55.087c2.317-.688 4.064-1.89 5.239-3.487 1.176-1.598 1.763-3.631 1.763-6.044 0-2.286-.549-4.314-1.646-6.054s-2.662-2.413-4.699-3.056c-2.037-.641-5.53-.446-10.48-.446h-15.459v20h16.587c4.042 0 6.939-.38 8.695-.913zM126 18.625c0-9.182-7.443-16.625-16.625-16.625h-91.75c-9.182 0-16.625 7.443-16.625 16.625v91.75c0 9.182 7.443 16.625 16.625 16.625h91.75c9.182 0 16.625-7.443 16.625-16.625v-91.75zm-35.447 66.12c-1.362 2.773-3.047 4.911-5.052 6.415-2.006 1.504-4.521 2.78-7.544 3.548-3.022.769-6.728 1.292-11.113 1.292h-27.844v-69h27.42c5.264 0 9.485.609 12.665 2.002 3.181 1.395 5.671 3.497 7.474 6.395 1.801 2.898 2.702 5.907 2.702 9.071 0 2.945-.8 5.708-2.397 8.308-1.598 2.602-4.011 4.694-7.237 6.292 4.166 1.222 7.37 3.304 9.61 6.248 2.24 2.945 3.36 6.422 3.36 10.432 0 3.227-.681 6.225-2.044 8.997z"></path> <title>Bootstrap</title> </svg> <svg viewBox="0 0 128 128" class="img-projects-tech" width="40"> <path fill="#F0DB4F" d="M1.408 1.408h125.184v125.185h-125.184z"></path><path fill="#323330" d="M116.347 96.736c-.917-5.711-4.641-10.508-15.672-14.981-3.832-1.761-8.104-3.022-9.377-5.926-.452-1.69-.512-2.642-.226-3.665.821-3.32 4.784-4.355 7.925-3.403 2.023.678 3.938 2.237 5.093 4.724 5.402-3.498 5.391-3.475 9.163-5.879-1.381-2.141-2.118-3.129-3.022-4.045-3.249-3.629-7.676-5.498-14.756-5.355l-3.688.477c-3.534.893-6.902 2.748-8.877 5.235-5.926 6.724-4.236 18.492 2.975 23.335 7.104 5.332 17.54 6.545 18.873 11.531 1.297 6.104-4.486 8.08-10.234 7.378-4.236-.881-6.592-3.034-9.139-6.949-4.688 2.713-4.688 2.713-9.508 5.485 1.143 2.499 2.344 3.63 4.26 5.795 9.068 9.198 31.76 8.746 35.83-5.176.165-.478 1.261-3.666.38-8.581zm-46.885-37.793h-11.709l-.048 30.272c0 6.438.333 12.34-.714 14.149-1.713 3.558-6.152 3.117-8.175 2.427-2.059-1.012-3.106-2.451-4.319-4.485-.333-.584-.583-1.036-.667-1.071l-9.52 5.83c1.583 3.249 3.915 6.069 6.902 7.901 4.462 2.678 10.459 3.499 16.731 2.059 4.082-1.189 7.604-3.652 9.448-7.401 2.666-4.915 2.094-10.864 2.07-17.444.06-10.735.001-21.468.001-32.237z"></path> <title>JavaScript</title> </svg>',
-    "projectDemoURL": "#",
-    "projectCodeURL": "#"
-  },
-  {
-    "projectTitle": "COVID-19 TRACKER",
-    "projectImage": "https://picsum.photos/400/200",
-    "projectGif": "img/covid-19-tracker.gif",
-    "projectDescription": "COVID-19 tracker using public APIs, with region options.",
-    "projectTechnologies" : '<svg viewBox="0 0 128 128" class="img-projects-tech" width="40"> <path fill="#E44D26" d="M19.037 113.876l-10.005-112.215h109.936l-10.016 112.198-45.019 12.48z"></path><path fill="#F16529" d="M64 116.8l36.378-10.086 8.559-95.878h-44.937z"></path><path fill="#EBEBEB" d="M64 52.455h-18.212l-1.258-14.094h19.47v-13.762h-34.511l.33 3.692 3.382 37.927h30.799zM64 88.198l-.061.017-15.327-4.14-.979-10.975h-13.817l1.928 21.609 28.193 7.826.063-.017z"></path><path fill="#fff" d="M63.952 52.455v13.763h16.947l-1.597 17.849-15.35 4.143v14.319l28.215-7.82.207-2.325 3.234-36.233.335-3.696h-3.708zM63.952 24.599v13.762h33.244l.276-3.092.628-6.978.329-3.692z"></path> <title>HTML5</title> </svg> <svg viewBox="0 0 128 128" class="img-projects-tech" width="40"> <path fill="#1572B6" d="M18.814 114.123l-10.054-112.771h110.48l-10.064 112.754-45.243 12.543-45.119-12.526z"></path><path fill="#33A9DC" d="M64.001 117.062l36.559-10.136 8.601-96.354h-45.16v106.49z"></path><path fill="#fff" d="M64.001 51.429h18.302l1.264-14.163h-19.566v-13.831h34.681999999999995l-.332 3.711-3.4 38.114h-30.95v-13.831z"></path><path fill="#EBEBEB" d="M64.083 87.349l-.061.018-15.403-4.159-.985-11.031h-13.882l1.937 21.717 28.331 7.863.063-.018v-14.39z"></path><path fill="#fff" d="M81.127 64.675l-1.666 18.522-15.426 4.164v14.39l28.354-7.858.208-2.337 2.406-26.881h-13.876z"></path><path fill="#EBEBEB" d="M64.048 23.435v13.831000000000001h-33.407999999999994l-.277-3.108-.63-7.012-.331-3.711h34.646zM64.001 51.431v13.831000000000001h-15.209l-.277-3.108-.631-7.012-.33-3.711h16.447z"></path> <title>CSS3</title> </svg> <svg viewBox="0 0 128 128" class="img-projects-tech" width="40"> <rect x="20" y="20" width="80" height="100" fill="white"/> <path fill="#5B4282" d="M75.701 65.603c-2.334-.768-5.694-.603-10.08-.603h-17.621v23h18.844c2.944 0 5.012-.315 6.203-.535 2.099-.376 3.854-1.104 5.264-1.982 1.409-.876 2.568-2.205 3.478-3.881.908-1.676 1.363-3.637 1.363-5.83 0-2.568-.658-4.54-1.975-6.436-1.316-1.896-3.141-2.965-5.476-3.733zM73.282 55.087c2.317-.688 4.064-1.89 5.239-3.487 1.176-1.598 1.763-3.631 1.763-6.044 0-2.286-.549-4.314-1.646-6.054s-2.662-2.413-4.699-3.056c-2.037-.641-5.53-.446-10.48-.446h-15.459v20h16.587c4.042 0 6.939-.38 8.695-.913zM126 18.625c0-9.182-7.443-16.625-16.625-16.625h-91.75c-9.182 0-16.625 7.443-16.625 16.625v91.75c0 9.182 7.443 16.625 16.625 16.625h91.75c9.182 0 16.625-7.443 16.625-16.625v-91.75zm-35.447 66.12c-1.362 2.773-3.047 4.911-5.052 6.415-2.006 1.504-4.521 2.78-7.544 3.548-3.022.769-6.728 1.292-11.113 1.292h-27.844v-69h27.42c5.264 0 9.485.609 12.665 2.002 3.181 1.395 5.671 3.497 7.474 6.395 1.801 2.898 2.702 5.907 2.702 9.071 0 2.945-.8 5.708-2.397 8.308-1.598 2.602-4.011 4.694-7.237 6.292 4.166 1.222 7.37 3.304 9.61 6.248 2.24 2.945 3.36 6.422 3.36 10.432 0 3.227-.681 6.225-2.044 8.997z"></path> <title>Bootstrap</title> </svg> <svg viewBox="0 0 128 128" class="img-projects-tech" width="40"> <path fill="#F0DB4F" d="M1.408 1.408h125.184v125.185h-125.184z"></path><path fill="#323330" d="M116.347 96.736c-.917-5.711-4.641-10.508-15.672-14.981-3.832-1.761-8.104-3.022-9.377-5.926-.452-1.69-.512-2.642-.226-3.665.821-3.32 4.784-4.355 7.925-3.403 2.023.678 3.938 2.237 5.093 4.724 5.402-3.498 5.391-3.475 9.163-5.879-1.381-2.141-2.118-3.129-3.022-4.045-3.249-3.629-7.676-5.498-14.756-5.355l-3.688.477c-3.534.893-6.902 2.748-8.877 5.235-5.926 6.724-4.236 18.492 2.975 23.335 7.104 5.332 17.54 6.545 18.873 11.531 1.297 6.104-4.486 8.08-10.234 7.378-4.236-.881-6.592-3.034-9.139-6.949-4.688 2.713-4.688 2.713-9.508 5.485 1.143 2.499 2.344 3.63 4.26 5.795 9.068 9.198 31.76 8.746 35.83-5.176.165-.478 1.261-3.666.38-8.581zm-46.885-37.793h-11.709l-.048 30.272c0 6.438.333 12.34-.714 14.149-1.713 3.558-6.152 3.117-8.175 2.427-2.059-1.012-3.106-2.451-4.319-4.485-.333-.584-.583-1.036-.667-1.071l-9.52 5.83c1.583 3.249 3.915 6.069 6.902 7.901 4.462 2.678 10.459 3.499 16.731 2.059 4.082-1.189 7.604-3.652 9.448-7.401 2.666-4.915 2.094-10.864 2.07-17.444.06-10.735.001-21.468.001-32.237z"></path> <title>JavaScript</title> </svg>',
-    "projectDemoURL": "#",
-    "projectCodeURL": "#"
-  }
-];
-
-projectFunction(data);
-
 // Modal
 
-let modal = document.getElementById('myModal');
-let modalHeading = document.getElementsByClassName('modal-title')[0];
-let modalBody = document.getElementsByClassName('modal-body')[0];
+let modal = document.getElementById("myModal");
+let modalHeading = document.getElementsByClassName("modal-title")[0];
+let modalBody = document.getElementsByClassName("modal-body")[0];
 
 function modalOpen(cardid) {
   let modalBodyReplacement = document.getElementById(cardid).innerHTML;
   modalBody.innerHTML = modalBodyReplacement;
-  let projectGif = data.map(a => a.projectGif);
-  document.getElementById('myModal').getElementsByClassName('card-img-top')[0].setAttribute("src", projectGif[cardid]);
-  document.getElementById('myModal').getElementsByClassName('card-img-top')[0].setAttribute("style", "transition: none; filter: none; transform: none;");
+  let projectGif = data.map((a) => a.projectGif);
+  document
+    .getElementById("myModal")
+    .getElementsByClassName("card-img-top")[0]
+    .setAttribute("src", projectGif[cardid]);
+  document
+    .getElementById("myModal")
+    .getElementsByClassName("card-img-top")[0]
+    .setAttribute("style", "transition: none; filter: none; transform: none;");
   modal.classList.add("d-block");
   setTimeout(() => {
     modal.classList.add("show");
@@ -134,21 +76,20 @@ const sliders = document.querySelectorAll(".slide-in");
 
 const appearOptions = {
   threshold: 0,
-  rootMargin: "0px 0px -200px 0px"
+  rootMargin: "0px 0px -200px 0px",
 };
 
-const appearOnScroll = new IntersectionObserver(function(
+const appearOnScroll = new IntersectionObserver(function (
   entries,
   appearOnScroll
 ) {
-  entries.forEach(entry => {
+  entries.forEach((entry) => {
     if (!entry.isIntersecting) {
       return;
     } else {
       entry.target.classList.remove("opacity-0");
       entry.target.classList.add("opacity-100");
-      if (entry.target.classList.contains("slide-in"))
-      {
+      if (entry.target.classList.contains("slide-in")) {
         entry.target.classList.add("appear");
       }
       appearOnScroll.unobserve(entry.target);
@@ -157,10 +98,10 @@ const appearOnScroll = new IntersectionObserver(function(
 },
 appearOptions);
 
-faders.forEach(fader => {
+faders.forEach((fader) => {
   appearOnScroll.observe(fader);
 });
 
-sliders.forEach(slider => {
+sliders.forEach((slider) => {
   appearOnScroll.observe(slider);
 });
--- js/preloader.js
+++ js/preloader.js
@@ -1,9 +1,9 @@
 var preloader = document.getElementsByClassName("preloader")[0];
-window.addEventListener('load', function() {
-    preloader.classList.remove("d-flex");
-    var elements = document.getElementsByClassName('d-none');
-    while (elements.length > 0 ) {
-        elements[0].classList.remove('d-none');
-    }
-    preloader.classList.add("d-none");
+window.addEventListener("load", function () {
+  preloader.classList.remove("d-flex");
+  var elements = document.getElementsByClassName("d-none");
+  while (elements.length > 0) {
+    elements[0].classList.remove("d-none");
+  }
+  preloader.classList.add("d-none");
 });
