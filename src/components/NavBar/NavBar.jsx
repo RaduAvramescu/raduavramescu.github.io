3e32f892a49539349722af5a021785ce4e34297f
RaduAvramescu <71271545+RaduAvramescu@users.noreply.github.com>
2020-12-02 15:47:08 +0200

    Made NavBar hide on scroll down and show on scroll up.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,75 +1,107 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 
-const NavBar = (props) => {
-  const [isCollapsed, setIsCollapsed] = useState(true);
+export default class NavBar extends Component {
+  constructor(props) {
+    super(props);
 
-  const handleCollapse = () => setIsCollapsed(!isCollapsed);
+    this.state = {
+      prevScrollPos: window.pageYOffset,
+      isVisible: true,
+      isCollapsed: true,
+    };
+  }
 
-  return (
-    <header id="header" className="fixed-top">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand font-weight-bold" href="/#">
-          RADU AVRAMESCU
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded={!isCollapsed ? true : false}
-          aria-label="Toggle navigation"
-          onClick={handleCollapse}
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
 
-        <div
-          className={`${isCollapsed ? "collapse" : ""} navbar-collapse`}
-          id="navbarSupportedContent"
-        >
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <a
-                className="nav-link px-0 mx-3"
-                href="/#"
-                onClick={handleCollapse}
-              >
-                HOME
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link px-0 mx-3"
-                href="#projects"
-                onClick={handleCollapse}
-              >
-                PROJECTS
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link px-0 mx-3"
-                href="#about"
-                onClick={handleCollapse}
-              >
-                ABOUT
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link px-0 mx-3"
-                href="#contact"
-                onClick={handleCollapse}
-              >
-                CONTACT
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </header>
-  );
-};
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
 
-export default NavBar;
+  handleScroll = () => {
+    const { prevScrollPos } = this.state;
+    const curScrollPos = window.pageYOffset;
+    const isVisible = prevScrollPos > curScrollPos;
+    this.setState({
+      prevScrollPos: curScrollPos,
+      isVisible,
+    });
+  };
+
+  handleCollapse = () =>
+    this.setState({ isCollapsed: !this.state.isCollapsed });
+
+  render() {
+    return (
+      <header
+        id="header"
+        className={`fixed-top ${!this.state.isVisible ? `header-hide` : ``}`}
+      >
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a className="navbar-brand font-weight-bold" href="/#">
+            RADU AVRAMESCU
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded={!this.state.isCollapsed ? true : false}
+            aria-label="Toggle navigation"
+            onClick={this.handleCollapse}
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div
+            className={`${
+              this.state.isCollapsed ? "collapse" : ""
+            } navbar-collapse`}
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a
+                  className="nav-link px-0 mx-3"
+                  href="/#"
+                  onClick={this.handleCollapse}
+                >
+                  HOME
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link px-0 mx-3"
+                  href="#projects"
+                  onClick={this.handleCollapse}
+                >
+                  PROJECTS
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link px-0 mx-3"
+                  href="#about"
+                  onClick={this.handleCollapse}
+                >
+                  ABOUT
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link px-0 mx-3"
+                  href="#contact"
+                  onClick={this.handleCollapse}
+                >
+                  CONTACT
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+}
