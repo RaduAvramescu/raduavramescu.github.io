96726ebf184cdec07c220cdf50b318dd8f898977
RaduAvramescu <71271545+RaduAvramescu@users.noreply.github.com>
2020-12-03 14:39:27 +0200

    Moved arrow-right CSS class to Jumbotron.module.css.

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -18,7 +18,10 @@ export default function Jumbotron() {
                 href="#projects"
               >
                 VIEW MY PROJECTS
-                <svg viewBox="0 0 64 64" className="ml-3 btn-icon arrow-right">
+                <svg
+                  viewBox="0 0 64 64"
+                  className={`ml-3 btn-icon ${styles["arrow-right"]}`}
+                >
                   <path
                     data-name="layer1"
                     d="M38.879 47.121l13.993-13.993.012-.012L55 31 38.879 14.879a3 3 0 0 0-4.242 4.242l8.879 8.88H13.999a3 3 0 0 0 0 6h29.515l-8.877 8.878a3 3 0 0 0 4.242 4.242z"
