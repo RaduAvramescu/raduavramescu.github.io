2a3114cdfacb7955f6b212b99711f0db1d4bd22f
Radu Avramescu <71271545+RaduAvramescu@users.noreply.github.com>
2021-03-29 14:40:53 +0300

    Changed function components to be arrow function components.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,67 +2,67 @@ import React from "react";
 
 import styles from "./Contact.module.css";
 
-export default function Contact() {
-  return (
-    <section id="contact">
-      <div className="section-padding">
-        <h2 className="section-title font-weight-bold text-center wow animate__animated animate__fadeIn">
-          CONTACT
-        </h2>
-        <div className="container wow animate__animated animate__fadeIn">
-          <form action="https://formspree.io/xwkrdeyq" method="POST">
-            <div className="form-row justify-content-between">
-              <div className={`col-md-5 form-group ${styles["form-group"]}`}>
-                <input
-                  className={`form-control ${styles["form-control"]}`}
-                  type="text"
-                  name="name"
-                  id="name"
-                  required
-                />
-                <label htmlFor="name" className={`${styles.label}`}>
-                  NAME
-                </label>
-              </div>
-
-              <div className={`col-md-5 form-group ${styles["form-group"]}`}>
-                <input
-                  className={`form-control ${styles["form-control"]}`}
-                  type="email"
-                  name="_replyto"
-                  id="email"
-                  required
-                  placeholder=" "
-                />
-                <label htmlFor="email" className={`${styles.label}`}>
-                  EMAIL
-                </label>
-              </div>
-
-              <div className={`col-md-12 form-group ${styles["form-group"]}`}>
-                <textarea
-                  className={`form-control ${styles["form-control"]}`}
-                  name="message"
-                  id="message"
-                  required
-                ></textarea>
-                <label htmlFor="message" className={`${styles.label}`}>
-                  MESSAGE
-                </label>
-              </div>
-
-              <div className="col-md-12 text-center">
-                <button
-                  type="submit"
-                  className={`btn btn-dark btn-lg ${styles["btn-submit"]}`}
-                >
-                  SUBMIT
-                </button>
-              </div>
+const Contact = () => (
+  <section id="contact">
+    <div className="section-padding">
+      <h2 className="section-title font-weight-bold text-center wow animate__animated animate__fadeIn">
+        CONTACT
+      </h2>
+      <div className="container wow animate__animated animate__fadeIn">
+        <form action="https://formspree.io/xwkrdeyq" method="POST">
+          <div className="form-row justify-content-between">
+            <div className={`col-md-5 form-group ${styles["form-group"]}`}>
+              <input
+                className={`form-control ${styles["form-control"]}`}
+                type="text"
+                name="name"
+                id="name"
+                required
+              />
+              <label htmlFor="name" className={`${styles.label}`}>
+                NAME
+              </label>
             </div>
-          </form>
-        </div>
+
+            <div className={`col-md-5 form-group ${styles["form-group"]}`}>
+              <input
+                className={`form-control ${styles["form-control"]}`}
+                type="email"
+                name="_replyto"
+                id="email"
+                required
+                placeholder=" "
+              />
+              <label htmlFor="email" className={`${styles.label}`}>
+                EMAIL
+              </label>
+            </div>
+
+            <div className={`col-md-12 form-group ${styles["form-group"]}`}>
+              <textarea
+                className={`form-control ${styles["form-control"]}`}
+                name="message"
+                id="message"
+                required
+              ></textarea>
+              <label htmlFor="message" className={`${styles.label}`}>
+                MESSAGE
+              </label>
+            </div>
+
+            <div className="col-md-12 text-center">
+              <button
+                type="submit"
+                className={`btn btn-dark btn-lg ${styles["btn-submit"]}`}
+              >
+                SUBMIT
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
-    </section>
-  );
-}
+    </div>
+  </section>
+);
+
+export default Contact;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,25 +2,25 @@ import React from "react";
 import ProjectsItem from "../ProjectsItem/ProjectsItem";
 import data from "../../data/data.json";
 
-export default function Projects() {
-  return (
-    <section id="projects">
-      <div className="section-padding text-center">
-        <h2 className="section-title font-weight-bold wow animate__animated animate__fadeIn">
-          PROJECTS
-        </h2>
-        <div className="container" id="projectsContent">
-          <div className="row">
-            {data.map((item, i) => (
-              <ProjectsItem
-                key={i}
-                data={data[i]}
-                delay={`animate__delay-${i}s`}
-              />
-            ))}
-          </div>
+const Projects = () => (
+  <section id="projects">
+    <div className="section-padding text-center">
+      <h2 className="section-title font-weight-bold wow animate__animated animate__fadeIn">
+        PROJECTS
+      </h2>
+      <div className="container" id="projectsContent">
+        <div className="row">
+          {data.map((item, i) => (
+            <ProjectsItem
+              key={i}
+              data={data[i]}
+              delay={`animate__delay-${i}s`}
+            />
+          ))}
         </div>
       </div>
-    </section>
-  );
-}
+    </div>
+  </section>
+);
+
+export default Projects;
