d78e7ce6bad13a219ea5f47bbd259b0b652a93eb
Radu Avramescu <contact@raduavramescu.com>
2025-09-07 16:54:01 +0300

    chore: use html5 and css3 consistently instead of using html and css as well

--- src/components/Technologies/Technologies.view.jsx
+++ src/components/Technologies/Technologies.view.jsx
@@ -1,36 +1,34 @@
 import React from "react";
 import TechnologyItem from "./components/TechnologyItem";
 
-const Technologies = ({ technologies, width }) => (
-  <>
-    {technologies.includes("HTML") && (
-      <TechnologyItem techName="HTML5" width={width}></TechnologyItem>
-    )}
-    {technologies.includes("CSS") && (
-      <TechnologyItem techName="CSS3" width={width}></TechnologyItem>
-    )}
-    {technologies.includes("JavaScript") && (
-      <TechnologyItem techName="JavaScript" width={width}></TechnologyItem>
-    )}
-    {technologies.includes("Bootstrap") && (
-      <TechnologyItem techName="Bootstrap" width={width}></TechnologyItem>
-    )}
-    {technologies.includes("Material-UI") && (
-      <TechnologyItem techName="Material-UI" width={width}></TechnologyItem>
-    )}
-    {technologies.includes("React") && (
-      <TechnologyItem techName="React" width={width}></TechnologyItem>
-    )}
-    {technologies.includes("GraphQL") && (
-      <TechnologyItem techName="GraphQL" width={width}></TechnologyItem>
-    )}
-    {technologies.includes("Git") && (
-      <TechnologyItem techName="Git" width={width}></TechnologyItem>
-    )}
-    {technologies.includes("npm") && (
-      <TechnologyItem techName="npm" width={width}></TechnologyItem>
-    )}
-  </>
-);
+const TECHNOLOGY_MAP = {
+  HTML5: "HTML5",
+  CSS3: "CSS3",
+  JavaScript: "JavaScript",
+  Bootstrap: "Bootstrap",
+  "Material-UI": "Material-UI",
+  React: "React",
+  GraphQL: "GraphQL",
+  Git: "Git",
+  npm: "npm",
+};
+
+const Technologies = ({ technologies, width }) => {
+  const techArray = technologies.split(" ");
+  
+  return (
+    <>
+      {techArray
+        .filter(tech => TECHNOLOGY_MAP[tech])
+        .map(tech => (
+          <TechnologyItem 
+            key={tech}
+            techName={TECHNOLOGY_MAP[tech]} 
+            width={width} 
+          />
+        ))}
+    </>
+  );
+};
 
 export default Technologies;
