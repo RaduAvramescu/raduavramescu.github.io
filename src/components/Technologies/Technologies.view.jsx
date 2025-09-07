dabd1b2ae67cb205cf10a08e9c93f025d0602aa4
Radu Avramescu <contact@raduavramescu.com>
2025-09-07 16:55:41 +0300

    chore: improve technologies view logic

--- src/components/Technologies/Technologies.view.jsx
+++ src/components/Technologies/Technologies.view.jsx
@@ -1,32 +1,18 @@
 import React from "react";
 import TechnologyItem from "./components/TechnologyItem";
 
-const TECHNOLOGY_MAP = {
-  HTML5: "HTML5",
-  CSS3: "CSS3",
-  JavaScript: "JavaScript",
-  Bootstrap: "Bootstrap",
-  "Material-UI": "Material-UI",
-  React: "React",
-  GraphQL: "GraphQL",
-  Git: "Git",
-  npm: "npm",
-};
-
 const Technologies = ({ technologies, width }) => {
   const techArray = technologies.split(" ");
-  
+
   return (
     <>
-      {techArray
-        .filter(tech => TECHNOLOGY_MAP[tech])
-        .map(tech => (
-          <TechnologyItem 
-            key={tech}
-            techName={TECHNOLOGY_MAP[tech]} 
-            width={width} 
-          />
-        ))}
+      {techArray.map((tech) => (
+        <TechnologyItem
+          key={tech}
+          techName={TECHNOLOGY_MAP[tech]}
+          width={width}
+        />
+      ))}
     </>
   );
 };
