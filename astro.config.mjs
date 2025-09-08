f323bdd6cd2efe2f35b4e4fcace76c38dbbba7b5
Radu Avramescu <contact@raduavramescu.com>
2025-09-08 14:57:56 +0300

    chore: remove bootstrap and add initial tailwind implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import tailwind from "@tailwindcss/vite";
 
 export default defineConfig({
   integrations: [icon()],
+  vite: {
+    plugins: [tailwind()],
+  },
 });
